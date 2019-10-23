530e6daa20c7b3072fa335e2502c2d352c02227d
tsubame-mz <rainy.garden777@gmail.com>
2019-10-24 00:50:10 +0900

    absl-py

--- RL/ppo.py
+++ RL/ppo.py
@@ -1,10 +1,11 @@
 import torch
 import numpy as np
 import gym
-import argparse
 import os
 import pickle
 from adabound import AdaBound
+from absl import app
+from absl import flags
 
 # from utils import _windows_enable_ANSI
 from model import PNet, VNet
@@ -12,38 +13,62 @@ from trajectory import Buffer, Trajectory
 from agent import Agent, Discriminator
 
 
-def train(
-    env,
-    agent,
-    n_sample_episodes,
-    n_train_epochs,
-    gamma,
-    lam,
-    eps,
-    batch_size,
-    n_opt_epochs,
-    clip_param,
-    v_loss_c,
-    ent_c,
-    max_grad_norm,
-    max_rew,
-    model_filename_base,
-    device,
-    use_discrim,
-    discrim,
-    discrim_filename_base,
-    expert_traj,
-):
+FLAGS = flags.FLAGS
+flags.DEFINE_string("env", "Taxi-v3", "gym env name")
+flags.DEFINE_bool("use_gpu", True, "using GPU")
+flags.DEFINE_integer("hid_num", 16, "hidden layer unit size")
+flags.DEFINE_float("lr", 1e-4, "learning rate")
+flags.DEFINE_float("final_lr", 1e-2, "final learning rate for AdaBound")
+flags.DEFINE_float("gamma", 0.99, "reward discount factor")
+flags.DEFINE_float("lam", 0.95, "GAE lambda")
+flags.DEFINE_float("eps", 1e-6, "small value for preventing 0 division in Advantage")
+flags.DEFINE_integer("batch_size", 16, "batch size for each training")
+flags.DEFINE_float("clip_param", 0.2, "PPO clipping liklihood ratio")
+flags.DEFINE_float("ent_c", 0.2, "entropy coefficient")
+flags.DEFINE_float("v_loss_c", 0.9, "value loss coefficient")
+flags.DEFINE_float("max_grad_norm", 0.5, "maximum gradient norm")
+flags.DEFINE_bool("use_discrim", True, "using AIRL Discriminator")
+
+flags.DEFINE_integer("n_sample_episodes", 10, "sampling episode in each training epoch")
+flags.DEFINE_integer("n_train_epochs", 10000, "training epoch")
+flags.DEFINE_integer("n_opt_epochs", 3, "update epoch in each iteration")
+flags.DEFINE_integer("n_test_epochs", 10, "test epoch")
+flags.DEFINE_bool("render_env", True, "env render in test epoch")
+
+flags.DEFINE_string("data_dir", "./data", "data save directory")
+# flags.DEFINE_string("data_dir", "/content/drive/My Drive/data", "data save directory")    # Google Drive
+flags.DEFINE_bool("resume", True, "load model parameters")
+
+
+def create_directory(dir_name):
+    if not os.path.exists(dir_name):
+        print("Create directory: {0}".format(dir_name))
+        os.mkdir(dir_name)
+
+
+def get_device(use_gpu):
+    # サポート対象のGPUがあれば使う
+    if use_gpu:
+        print("Check GPU available")
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    else:
+        device = torch.device("cpu")
+    if device == "cuda":
+        torch.backends.cudnn.benchmark = True
+    return device
+
+
+def train(env, agent, max_rew, model_filename_base, device, discrim, discrim_filename_base, expert_traj):
     print("Train start")
     try:
-        for epoch in range(n_train_epochs):
+        for epoch in range(FLAGS.n_train_epochs):
             # 評価モード
             agent.eval()
             traj = Trajectory()
             episode_rewards = []
-            if use_discrim:
+            if discrim is not None:
                 real_episode_rewards = []
-            for episode in range(n_sample_episodes):
+            for episode in range(FLAGS.n_sample_episodes):
                 ob = env.reset()
                 done = False
                 buf = Buffer()
@@ -53,7 +78,7 @@ def train(
                     next_ob, reward, done, info = env.step(action)
                     buf_data = dict(obs=ob, actions=action, rewards=reward, values=value, llhs=llh)
 
-                    if use_discrim:
+                    if discrim is not None:
                         pseudo_rew = discrim.get_pseudo_reward(ob_tsr)
                         buf_data["real_rewards"] = buf_data["rewards"]
                         buf_data["rewards"] = pseudo_rew
@@ -63,26 +88,32 @@ def train(
                     buf.append(buf_data)
                     ob = next_ob
 
-                buf.finish_path(gamma, lam)
+                buf.finish_path(FLAGS.gamma, FLAGS.lam)
                 traj.append(buf.data_map)
                 episode_rewards.append(buf.data_map["rewards"].sum())
 
-                if use_discrim:
+                if discrim is not None:
                     real_episode_rewards.append(buf.data_map["real_rewards"].sum())
 
             # 学習モード
             agent.train()
-            traj.finish_path(eps, device)
+            traj.finish_path(FLAGS.eps, device)
             pol_loss, v_loss, entropy = agent.update(
-                traj, batch_size, n_opt_epochs, clip_param, v_loss_c, ent_c, max_grad_norm
+                traj,
+                FLAGS.batch_size,
+                FLAGS.n_opt_epochs,
+                FLAGS.clip_param,
+                FLAGS.v_loss_c,
+                FLAGS.ent_c,
+                FLAGS.max_grad_norm,
             )
-            if use_discrim:
+            if discrim is not None:
                 discrim.train()
-                discrim_a_loss, discrim_e_loss = discrim.update(traj, expert_traj, batch_size, gamma, agent)
+                discrim_a_loss, discrim_e_loss = discrim.update(traj, expert_traj, FLAGS.batch_size, FLAGS.gamma, agent)
 
             mean_rew = np.mean(episode_rewards)
             print("Epoch[{:3d}], ".format(epoch + 1), end="")
-            if use_discrim:
+            if discrim is not None:
                 print(
                     "Loss(P/V/E/Da/De)[{:+.6f}/{:+.6f}/{:+.6f}/{:+.6f}/{:+.6f}], ".format(
                         pol_loss, v_loss, entropy, discrim_a_loss, discrim_e_loss
@@ -93,7 +124,7 @@ def train(
                 print("Loss(P/V/E)[{:+.6f}/{:+.6f}/{:+.6f}], ".format(pol_loss, v_loss, entropy), end="")
             reward_format = "Reward(Mean/Min/Max)[{:+3.3f}/{:+3.3f}/{:+3.3f}]"
             print(reward_format.format(mean_rew, np.min(episode_rewards), np.max(episode_rewards)), end="")
-            if use_discrim:
+            if discrim is not None:
                 mean_real_rew = np.mean(real_episode_rewards)
                 reward_format = ", Reward(Real)(Mean/Min/Max)[{:+3.3f}/{:+3.3f}/{:+3.3f}]"
                 print(
@@ -109,7 +140,7 @@ def train(
                 save_info = dict(max_rew=max_rew)
                 agent.save_model(model_filename_base, save_info, "best")
 
-                if use_discrim:
+                if discrim is not None:
                     discrim.save_model(discrim_filename_base, "best")
 
             if ((epoch + 1) % 100) == 0:
@@ -117,7 +148,7 @@ def train(
                 save_info = dict(max_rew=max_rew)
                 agent.save_model(model_filename_base, save_info, "checkpoint")
 
-                if use_discrim:
+                if discrim is not None:
                     discrim.save_model(discrim_filename_base, "checkpoint")
 
     except KeyboardInterrupt:
@@ -128,22 +159,22 @@ def train(
     save_info = dict(max_rew=max_rew)
     agent.save_model(model_filename_base, save_info, "last")
 
-    if use_discrim:
+    if discrim is not None:
         discrim.save_model(discrim_filename_base, "last")
 
 
-def test(env, agent, n_test_epochs, render_env, device):
+def test(env, agent, device):
     print("Test start")
     try:
         # 評価モード
         agent.eval()
         test_reward = []
-        for epoch in range(n_test_epochs):
+        for epoch in range(FLAGS.n_test_epochs):
             ob = env.reset()
             done = False
             step = 0
             episode_reward = 0
-            if render_env:
+            if FLAGS.render_env:
                 print("-- env[{0:02d}:{1:03d}] ".format(epoch + 1, step) + "-" * 30)
                 env.render()
 
@@ -155,7 +186,7 @@ def test(env, agent, n_test_epochs, render_env, device):
                 ob = next_ob
 
                 step += 1
-                if render_env:
+                if FLAGS.render_env:
                     print("-- env[{0:02d}:{1:03d}] ".format(epoch + 1, step) + "-" * 30)
                     env.render()
                     print(info, (pi * 1000).astype(int), "{:.6f}".format(value))
@@ -171,67 +202,29 @@ def test(env, agent, n_test_epochs, render_env, device):
     print("Test complete")
 
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--env", type=str, default="Taxi-v3")
-    parser.add_argument("--use_gpu", type=bool, default=True)
-    parser.add_argument("--hid_num", type=int, default=16)
-    parser.add_argument("--lr", type=float, default=1e-4)
-    parser.add_argument("--final_lr", type=float, default=1e-2)
-    parser.add_argument("--gamma", type=float, default=0.99)
-    parser.add_argument("--lam", type=float, default=0.95)
-    parser.add_argument("--eps", type=float, default=1e-6)
-    parser.add_argument("--batch_size", type=int, default=64)
-    parser.add_argument("--clip_param", type=float, default=0.2)
-    parser.add_argument("--ent_c", type=float, default=0.2)
-    parser.add_argument("--v_loss_c", type=float, default=0.9)
-    parser.add_argument("--max_grad_norm", type=float, default=0.5)
-    parser.add_argument("--use_discrim", type=bool, default=False)
-
-    parser.add_argument("--n_sample_episodes", type=int, default=10)
-    parser.add_argument("--n_train_epochs", type=int, default=10000)
-    parser.add_argument("--n_opt_epochs", type=int, default=3)
-    parser.add_argument("--n_test_epochs", type=int, default=10)
-    parser.add_argument("--render_env", type=bool, default=True)
-
-    parser.add_argument("--data_dir", type=str, default="data")
-    # parser.add_argument("--data", type=str, default="/content/drive/My Drive/data")
-    parser.add_argument("--resume", type=bool, default=True)
-
-    args = parser.parse_args(args=[])
-
-    # サポート対象のGPUがあれば使う
-    if args.use_gpu:
-        print("Check GPU available")
-        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    else:
-        device = torch.device("cpu")
-    if device == "cuda":
-        torch.backends.cudnn.benchmark = True
+def main(_):
+    device = get_device(FLAGS.use_gpu)
     print("Use device: {}".format(device))
 
     # # モデル保存用フォルダ生成
-    if not os.path.exists(args.data_dir):
-        print("Create directory: {0}".format(args.data_dir))
-        os.mkdir(args.data_dir)
-    if not os.path.exists(os.path.join(args.data_dir, "models")):
-        print("Create directory: {0}".format(os.path.join(args.data_dir, "models")))
-        os.mkdir(os.path.join(args.data_dir, "models"))
+    data_dir = FLAGS.data_dir
+    create_directory(data_dir)
+    create_directory(os.path.join(data_dir, "models"))
 
-    env = gym.make(args.env)
+    env = gym.make(FLAGS.env)
 
-    p_net = PNet(env.observation_space, env.action_space, args.hid_num)
-    v_net = VNet(env.observation_space, args.hid_num)
+    p_net = PNet(env.observation_space, env.action_space, FLAGS.hid_num)
+    v_net = VNet(env.observation_space, FLAGS.hid_num)
     print(p_net)
     print(v_net)
     p_net.to(device)
     v_net.to(device)
-    optim_p = AdaBound(p_net.parameters(), lr=args.lr, final_lr=args.final_lr)
-    optim_v = AdaBound(v_net.parameters(), lr=args.lr, final_lr=args.final_lr)
+    optim_p = AdaBound(p_net.parameters(), lr=FLAGS.lr, final_lr=FLAGS.final_lr)
+    optim_v = AdaBound(v_net.parameters(), lr=FLAGS.lr, final_lr=FLAGS.final_lr)
     agent = Agent(p_net, v_net, optim_p, optim_v, device)
 
-    if args.use_discrim:
-        expert_filename = os.path.join(args.data_dir, "expert_data", "taxi_expert.pkl")
+    if FLAGS.use_discrim:
+        expert_filename = os.path.join(FLAGS.data_dir, "expert_data", "taxi_expert.pkl")
         print("Load expert data: ", expert_filename)
         with open(expert_filename, "rb") as f:
             expert_traj = Trajectory()
@@ -241,14 +234,14 @@ def main():
                 expert_traj.append(epi)
             expert_traj.to_tensor(device)
 
-        pseudo_rew_net = VNet(env.observation_space, args.hid_num)
-        shaping_val_net = VNet(env.observation_space, args.hid_num)
+        pseudo_rew_net = VNet(env.observation_space, FLAGS.hid_num)
+        shaping_val_net = VNet(env.observation_space, FLAGS.hid_num)
         print(pseudo_rew_net)
         print(shaping_val_net)
         pseudo_rew_net.to(device)
         shaping_val_net.to(device)
         optim_discrim = AdaBound(
-            list(pseudo_rew_net.parameters()) + list(shaping_val_net.parameters()), lr=args.lr, final_lr=args.final_lr
+            list(pseudo_rew_net.parameters()) + list(shaping_val_net.parameters()), lr=FLAGS.lr, final_lr=FLAGS.final_lr
         )
         discrim = Discriminator(pseudo_rew_net, shaping_val_net, optim_discrim, device)
     else:
@@ -257,8 +250,8 @@ def main():
 
     # モデルの途中状態を読込む
     max_rew = -1e6
-    model_filename_base = os.path.join(args.data_dir, "models", "model_" + args.env + "_PPO_H" + str(args.hid_num))
-    if args.resume:
+    model_filename_base = os.path.join(FLAGS.data_dir, "models", "model_" + FLAGS.env + "_PPO_H" + str(FLAGS.hid_num))
+    if FLAGS.resume:
         print("Load last model")
         load_info = agent.load_model(model_filename_base, "last")
         if load_info:
@@ -267,40 +260,18 @@ def main():
         else:
             print("Model file not found")
 
-        if args.use_discrim:
+        if FLAGS.use_discrim:
             discrim_filename_base = os.path.join(
-                args.data_dir, "models", "discrim_" + args.env + "_AIRL_H" + str(args.hid_num)
+                FLAGS.data_dir, "models", "discrim_" + FLAGS.env + "_AIRL_H" + str(FLAGS.hid_num)
             )
             discrim.load_model(discrim_filename_base, "last")
         else:
             discrim_filename_base = None
 
-    train(
-        env,
-        agent,
-        args.n_sample_episodes,
-        args.n_train_epochs,
-        args.gamma,
-        args.lam,
-        args.eps,
-        args.batch_size,
-        args.n_opt_epochs,
-        args.clip_param,
-        args.v_loss_c,
-        args.ent_c,
-        args.max_grad_norm,
-        max_rew,
-        model_filename_base,
-        device,
-        args.use_discrim,
-        discrim,
-        discrim_filename_base,
-        expert_traj,
-    )
-
-    test(env, agent, args.n_test_epochs, args.render_env, device)
+    train(env, agent, max_rew, model_filename_base, device, discrim, discrim_filename_base, expert_traj)
+    test(env, agent, device)
 
 
 if __name__ == "__main__":
     # _windows_enable_ANSI()
-    main()
+    app.run(main)
